1c11dfbb5746c4b9ffa6429d4258789b7553d831
AlizaKhawar <elizakhawar@gmail.com>
2023-12-21 11:48:42 +0000

    second star

--- exercises/arrayMain.c
+++ exercises/arrayMain.c
@@ -18,3 +18,23 @@ int main(int argc, char **argv) {
   int sum = getSum(arr, n);
   printf("sum of elements = %d\n", sum);
   printArray(arr, n);
+
+   free(arr);
+
+
+int size;
+printf("\nEnter Size of your array : ");
+scanf("%d",&size);
+
+int array[size];
+
+printf("\nEnter elements of your array with wide spaces : ");
+
+for(int i=0;i<size;i++){
+
+scanf("%d",&array[i]);
+
+}
+
+
+//testing getmean(); function
